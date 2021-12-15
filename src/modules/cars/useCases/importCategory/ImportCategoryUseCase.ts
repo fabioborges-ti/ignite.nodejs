93b7815ebb3e55feee1650b2b039cec6d09e9abb
fabioborges-ti <fabioborges.ti@gmail.com>
2021-12-14 23:44:45 -0300

    .

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,16 @@
+import fs from 'fs';
+import { parse } from 'csv-parse';
+
 class ImportCategoryUseCase {
-  execute(file: unknown) {
-    console.log(file);
+  execute(file?: Express.Multer.File): void {
+    if (file) {
+      const stream = fs.createReadStream(file.path);
+      const parseFile = parse();
+      stream.pipe(parseFile);
+      parseFile.on('data', async line => {
+        console.log(line);
+      });
+    }
   }
 }
 
